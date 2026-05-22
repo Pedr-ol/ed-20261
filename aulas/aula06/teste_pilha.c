700cd02bcb567db3f516ca07bd02bb9c3a27c686
Pedro Oliveira <pedrooliver1012@gmail.com>
2026-05-22 10:57:30 -0300

    Adicionando aula06

---
+++ aulas/aula06/teste_pilha.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "pilha.h"
+
+int main()
+{
+    Pilha *p = criar();
+
+    empilhar(p, 1);
+    empilhar(p, 8);
+    empilhar(p, 0);
+
+    No *no = p->topo;
+    while (no != NULL)
+    {
+        printf("%i ->", no->dados);
+        no = no->proximo;
+    }
+    printf("\n");
+
+    desempilhar(p);
+    no = p->topo;
+    while (no != NULL)
+    {
+        printf("%i ->", no->dados);
+        no = no->proximo;
+    }
+    printf("\n");
+
+    empilhar(p, 6);
+    no = p->topo;
+    while (no != NULL)
+    {
+        printf("%i ->", no->dados);
+        no = no->proximo;
+    }
+    printf("\n");
+
+    desempilhar(p);
+    no = p->topo;
+    while (no != NULL)
+    {
+        printf("%i ->", no->dados);
+        no = no->proximo;
+    }
+    printf("\n");
+
+    desempilhar(p);
+    no = p->topo;
+    while (no != NULL)
+    {
+        printf("%i ->", no->dados);
+        no = no->proximo;
+    }
+    printf("\n");
+
+    destruir(p);
+
+    return 0;
+}
